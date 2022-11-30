c98828c68d0b3c8ddc4ac76c798e250bac24aa3a
Hold <95875067+HoldCodes@users.noreply.github.com>
2022-11-30 20:32:42 -0300

    Add files via upload

--- TrabalhoSo/janelas.py
+++ TrabalhoSo/janelas.py
@@ -84,20 +84,96 @@ class JanelaSistema:
     def __init__(self):
         self.window = tkinter.Tk()
 
-        self.window.geometry("800x300")
+        self.window.geometry("800x500")
 
-        self.frame = Frame(self.window)
-        self.frame.pack()
         ### variaveis ###
+        self.boot_time_timestamp = psutil.boot_time()
+        self.bt = datetime.fromtimestamp(self.boot_time_timestamp)
+
+        self.cpufreq = psutil.cpu_freq()
+
+        self.svmem = psutil.virtual_memory()
+        self.swap = psutil.swap_memory()
+
+        self.partitions = psutil.disk_partitions()
+        self.partition_usage = NONE
+        self.disk_io = psutil.disk_io_counters()
+
+        self.if_addrs = psutil.net_if_addrs()
+        self.net_io = psutil.net_io_counters()
 
         ### Informacoes do sistema ###
-        self.txt = Text(self.window, bg="#404040", fg="white", insertbackground="white", font=("times", 14))
-        self.txt.insert(INSERT, f"System: {platform.system()}\n")
-        self.txt.insert(INSERT, f"Node Name: {platform.node()}\n")
-        self.txt.insert(INSERT, f"Release: {platform.release()}\n")
-        self.txt.insert(INSERT, f"Version: {platform.version()}\n")
-        self.txt.insert(INSERT, f"Machine: {platform.machine()}\n")
-        self.txt.insert(INSERT, f"Processor: {platform.processor()}\n")
+        self.txt = Text(self.window, bg="#404040", fg="white", font=("arial", 11), height=300, width=300)
+        self.txt.insert(INSERT, f"========== System Information ==========\n"
+                                f"System:  {platform.system()}\n"
+                                f"Node Name:  {platform.node()}\n"
+                                f"Release:  {platform.release()}\n"
+                                f"Version:  {platform.version()}\n"
+                                f"Machine:  {platform.machine()}\n"
+                                f"Processor:  {platform.processor()}\n")
+
+        self.txt.insert(INSERT, f"========== Boot Time: ==========\n"
+                                f"Boot time:  {self.bt.year}/{self.bt.month}/{self.bt.day} "
+                                f"{self.bt.hour}:{self.bt.minute}:{self.bt.second}\n")
+
+        self.txt.insert(INSERT, f"========== CPU Information: ==========\n"
+                                f"Physical Cores:  {psutil.cpu_count(logical=False)}\n"
+                                f"Total Cores:  {psutil.cpu_count(logical=True)}\n"
+                                f"Max Frequency:  {self.cpufreq.max:.2f}Mhz\n"
+                                f"Min Frequency:  {self.cpufreq.min:.2f}Mhz\n"
+                                f"Current Frequency:  {self.cpufreq.current:.2f}Mhz\n"
+                                f"CPU Usage Per Core: \n")
+        for i, percentage in enumerate(psutil.cpu_percent(percpu=True)):
+            self.txt.insert(INSERT, f"Core {i}:  {percentage}%\n")
+        self.txt.insert(INSERT, f"Total CPU Usage:  {psutil.cpu_percent()}%\n")
+
+        self.txt.insert(INSERT, f"========== Memory Usage: ==========\n"
+                                f"Total:  {self.get_size(self.svmem.total)}\n"
+                                f"Available:  {self.get_size(self.svmem.available)}\n"
+                                f"Used:  {self.get_size(self.svmem.used)}\n"
+                                f"Percentage:  {self.get_size(self.svmem.percent)}%\n"
+                                f"===== Swap =====\n"
+                                f"Total:  {self.get_size(self.swap.total)}\n"
+                                f"Free:  {self.get_size(self.swap.free)}\n"
+                                f"Used:  {self.get_size(self.swap.used)}\n"
+                                f"Percentage:  {self.get_size(self.swap.percent)}%\n")
+
+        self.txt.insert(INSERT, f"========== Disk Usage: ==========\n"
+                                f"Partitions and Usage: \n")
+
+        for partition in self.partitions:
+            self.txt.insert(INSERT, f"=== Device: {partition.device} ===\n"
+                                    f"  Mountpoint: {partition.mountpoint}\n"
+                                    f"  File system type: {partition.fstype}\n")
+
+            try:
+                self.partition_usage = psutil.disk_usage(partition.mountpoint)
+            except PermissionError:
+                continue
+
+            self.txt.insert(INSERT, f"  Total Size: {self.get_size(self.partition_usage.total)}\n"
+                                    f"  Used: {self.get_size(self.partition_usage.used)}\n"
+                                    f"  Free: {self.get_size(self.partition_usage.free)}\n"
+                                    f"  Percentage: {self.get_size(self.partition_usage.percent)}%\n")
+
+        self.txt.insert(INSERT, f"Total read: {self.get_size(self.disk_io.read_bytes)}\n"
+                                f"Total write: {self.get_size(self.disk_io.write_bytes)}\n")
+
+        self.txt.insert(INSERT, f"========== Network Information: ==========\n")
+        for interface_name, interface_addresses in self.if_addrs.items():
+            for address in interface_addresses:
+                self.txt.insert(INSERT, f"=== Interface: {interface_name} ===\n")
+                if str(address.family) == 'AddressFamily.AF_INET':
+                    self.txt.insert(INSERT, f"  IP Address: {address.address}\n"
+                                            f"  Netmask: {address.netmask}\n"
+                                            f"  Broadcast IP: {address.broadcast}\n")
+                elif str(address.family) == 'AddressFamily.AF_PACKET':
+                    self.txt.insert(INSERT, f"  MAC Address: {address.address}\n"
+                                            f"  Netmask: {address.netmask}\n"
+                                            f"  Broadcast MAC: {address.broadcast}\n")
+        self.txt.insert(INSERT, f"Total Bytes Sent: {self.get_size(self.net_io.bytes_sent)}\n"
+                                f"Total Bytes Received: {self.get_size(self.net_io.bytes_recv)}\n")
+
         self.txt.pack()
 
     def get_size(self, bytes, suffix="B"):
